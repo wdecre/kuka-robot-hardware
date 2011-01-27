ea545ccfff3585fffa8d9c676ac562f10d530166
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-01-27 10:00:09 +0100

    add event OutputPort to FRI

--- src/FRIComponent.hpp
+++ src/FRIComponent.hpp
@@ -77,6 +77,11 @@ private:
 	//Eigen::Matrix<double,7,7> m_massTmp; Not correct so useless
 
 	/**
+	 * events
+	 */
+	OutputPort<std::string> m_events;
+
+	/**
 	 * statistics
 	 */
 	OutputPort<tFriRobotState> m_RobotStatePort;
@@ -109,7 +114,7 @@ private:
 
 	const char* m_remote_address;
 	struct sockaddr m_remote_addr;
-	uint16_t counter;
+	uint16_t counter, fri_state_last;
 };
 
 }//Namespace LWR
